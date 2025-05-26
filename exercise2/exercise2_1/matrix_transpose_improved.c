08daa72d578345e6f677c6ab2f9872392fea4ebb
Aaron <h44114025@gs.ncku.edu.tw>
2025-05-26 13:39:15 +0800

    matrix_transpose completed_v1.0

--- exercise2/exercise2_1/matrix_transpose_improved.c
+++ exercise2/exercise2_1/matrix_transpose_improved.c
@@ -1,3 +1,11 @@
 void matrix_transpose(int n, int *dst, int *src) {
-    // Inplement your code here
+    for (int v = 0; v < n/8; v++) {
+        for (int u = 0; u < n/8; u++) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    dst[v*8 + u*8*n + i*n + j] = src[8*n*v + 8*u + n*j + i];
+                }
+            }
+        }
     }
+}
